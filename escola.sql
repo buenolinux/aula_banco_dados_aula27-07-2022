441a36e64fe72784a740640a20ef3b0e9d84458f
André <108432366+buenolinux@users.noreply.github.com>
2022-07-27 14:18:40 -0300

    Update escola.sql

--- escola.sql
+++ escola.sql
@@ -23,3 +23,24 @@ CREATE TABLE tab_salas(
 	capacidade INT NOT NULL,
 	equipamento VARCHAR(3)NOT NULL
 );
+
+CREATE TABLE tab_turmas(
+	id_turma INT NOT NULL PRIMARY KEY IDENTITY,
+	nome VARCHAR(30) NOT NULL,
+	id_aluno INT NOT NULL,
+	id_professor INT NOT NULL,
+	id_sala INT NOT NULL,
+
+	CONSTRAINT fk_id_aluno
+	FOREIGN KEY(id_aluno)
+	REFERENCES tab_alunos(id_aluno),
+
+	CONSTRAINT fk_id_professor
+	FOREIGN KEY (id_professor)
+	REFERENCES tab_professores(id_professor),
+
+	CONSTRAINT fk_id_sala
+	FOREIGN KEY(id_sala)
+	REFERENCES tab_salas(id_sala)
+
+);
